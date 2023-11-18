540f5ab80dce9b717c4e1f11914b72eb170d679a
Jonathan Prichard <66090077+Jprichard314@users.noreply.github.com>
2023-11-18 08:18:30 -0500

    added month capture query

--- dataCapture/apis.R
+++ dataCapture/apis.R
@@ -38,3 +38,36 @@ getData_phlCartoApi <- function(query,format = 'CSV'){
   mutate_all(na_if,"NULL")
   return(data)
 }
+
+
+getData_cartoDbMonthQuery <- function(
+    baseQuery
+    , monthsOfData = 12
+) 
+{
+  #'Purpose:
+  #'  This function wraps a sql query written to Philadelphia's CartoDB Api because
+  #'  url encoding doesn't seem to work well with Postgres's interval syntax.
+  #'  
+  #'  It takes today's date and captures back a number of months determined by the user.
+  #'Input
+  #'  baseQuery    -- Select and from statement for 
+  #'  monthsOfData -- defaults to 12, but the number of months back we're capturing.
+  #'Output
+  #'  X months of 311 data.
+  
+  # calculate query date.
+  start_date <- floor_date(
+    Sys.Date()
+    , unit = 'month'
+  ) - months(monthsOfData)
+  
+  # Write query using glue.
+  query <-  glue(
+    "{baseQuery} '{start_date}'"
+  )
+  
+  # get data
+  temp <- getData_phlCartoApi( query )
+  return(temp)
+}
